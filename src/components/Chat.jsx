67dfc76039e16a6e0af4eeeac03651e040cbefd4
DAvalos36 <davalos@itparral.edu.mx>
2023-11-09 08:59:02 -0600

    feat: chatbot funcional

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,13 +1,17 @@
 /* eslint-disable react/prop-types */
-import { Input, ScrollShadow } from "@nextui-org/react";
+import { Button, Input, ScrollShadow } from "@nextui-org/react";
 import BotEscribiendo from "./BotEscribiendo";
 import { useState } from "react";
+import axios from "axios";
 
 function Mensaje(props) {
-	const temp = parseInt(props.id, 10);
-	const bot = temp % 2 === 0;
+	const role = props.role;
+	const content = props.content;
+	const bot = role === "assistant";
 
 	const color = bot ? "bg-default" : "bg-primary";
+
+	if (role === "system") return null;
 
 	return (
 		<div
@@ -16,33 +20,89 @@ function Mensaje(props) {
 				bot ? "self-start" : "self-end"
 			} mt-3 break-all ${color} p-2 rounded-lg text-white max-w-[80%]`}
 		>
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse minima
-			repellat laborum quibusdam optio atque? Facilis aut atque itaque
-			voluptatem, amet, laboriosam rerum laudantium doloribus minima quis quod
-			eos {props.id.toString()} totam?
+			{content}
 		</div>
 	);
 }
 
-const arr = [1, 2, 3, 4, 5, 6, 7, 8];
-
 function Chat() {
+	const [cargando, setCargando] = useState(false);
 	const [conversacion, setConversacion] = useState([]);
+
+	const [inputValue, setInputValue] = useState("");
+
+	// use axios to send message to backend
+
+	async function sendMessage(chatHistory, message) {
+		setInputValue("");
+		setCargando(true);
+		const m = { role: "user", content: message };
+		if (chatHistory.length === 0) {
+			const u = {
+				role: "system",
+				content:
+					"Eres un gia de turista de una ciudad llamada hidalgo del parral, en el estado de chihuahua, Mexico. Como experto que eres, tienen un gran conocimiento historico, y eres capaz de resolver cualquier duda relacionada",
+			};
+			chatHistory.push(u);
+		}
+		setConversacion([...conversacion, m]);
+		chatHistory.push(m);
+		console.log(chatHistory);
+		try {
+			// const response = await axios.post("http://127.0.0.1:8000/chatbot/", {
+			// 	chatHistory,
+			// });
+			const response = await axios.post(
+				"http://127.0.0.1:8000/chatbot/",
+
+				chatHistory,
+
+				{ headers: { "Access-Control-Allow-Origin": "*" } },
+			);
+			const data = response.data;
+			console.log(data);
+			setConversacion(data.respuesta);
+		} catch (error) {
+			console.log(error);
+			alert(error);
+		} finally {
+			setCargando(false);
+		}
+	}
 
 	return (
 		<div className="h-60 border flex flex-col max-w-md">
-			<div className="bg-orange-400 p-3">
+			<div className="bg-gray-600 p-3">
 				<ScrollShadow hideScrollBar className="flex flex-col h-[400px] ">
 					{/* Mensajes aqui */}
-					{arr.map((id, i) => (
-						<Mensaje id={i} key={id} />
+					{conversacion.map((e, i) => (
+						// rome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+						<Mensaje role={e.role} content={e.content} key={i} />
 					))}
 				</ScrollShadow>
 				<BotEscribiendo />
 			</div>
-			<div className="bg-blue-400 p-1">
+			<div className="bg-black p-3 flex justify-around items-center">
 				{/* Input aqui */}
-				<Input type="text" label="Escribe tus dudas aqui!" />
+				<Input
+					value={inputValue}
+					onChange={(e) => setInputValue(e.target.value)}
+					type="text"
+					className="w-[75%]"
+					label="¡Escribe tus dudas aqui!"
+				/>
+				<Button
+					color="primary"
+					className="w-[10%] h-full"
+					isLoading={cargando}
+					isDisabled={inputValue === ""}
+					onClick={() =>
+						// Get the input data
+						sendMessage(conversacion, inputValue)
+					}
+				>
+					E
+				</Button>
 			</div>
 		</div>
 	);
