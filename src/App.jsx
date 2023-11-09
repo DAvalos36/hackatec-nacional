d882b1737444b318ae8e77c80e8114a836265b14
DAvalos36 <davalos@itparral.edu.mx>
2023-11-08 19:15:03 -0600

    PUNTO DE PARTIDA

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "@nextui-org/react";
+
+function App() {
+	return (
+		<>
+			<div className="">
+				<h1>React App</h1>
+				<Button color="primary">Button</Button>
+			</div>
+		</>
+	);
+}
+
+export default App;
